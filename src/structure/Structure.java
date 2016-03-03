9d431f46b26d2c9e32c058b8368a68cadb13ab4e
swati <swati.bhattu616@gmail.com>
2016-03-03 14:49:16 +0530

    my first commit

---
+++ src/structure/Structure.java
@@ -0,0 +1,17 @@
+package structure;
+
+import java.io.Console;
+import static structure.example.a;
+public class Structure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println(a);
+		
+	}
+		
+	
+}
+	
+
---
+++ src/structure/example.java
@@ -0,0 +1,6 @@
+package structure;
+
+public class example {
+
+	public static int a=10;
+}
